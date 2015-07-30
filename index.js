a77e0a3fa44225ced423683fafeef6d51d54e923
Matthew Elphick <matthew.elphick@clock.co.uk>
2015-07-30 09:49:30 +0100

    Work in progress on findModule

--- index.js
+++ index.js
@@ -11,37 +11,36 @@ module.exports = function () {
       var possiblePackages = []
         , options
 
-      async.forEach(repos, function (repo, callback) {
-
-        options = {
-          user: repo.login
-        , repo: repo.repo
-        }
-
-        possiblePackages.concat(finder.getDependencies(options, callback))
-
-      }, function(err) {
+      console.log(1, repos)
+      async.map(repos, finder.getDependencies, function (err, deps) {
         console.log(err)
-
-        //TODO Handle getDependecies error
-        //if(err) cb(new Error('Could not get possible packages'))
-
-        finder.getClockMembersList(function (err, members) {
-
-          function memberPackages(element) {
-            var repoUrlParts = element.split('/')
-              , repoUser = repoUrlParts[repoUrlParts.length - 2]
-            return members.indexOf(repoUser) > -1
-          }
-
-          actualPackages = possiblePackages.filter(memberPackages)
-          console.log(possiblePackages)
-
-          cb(null, actualPackages)
-        
-        })
-
+        console.log(deps)
       })
+
+      //   possiblePackages.concat(finder.getDependencies(options, callback))
+
+      // }, function(err) {
+      //   console.log(2, err)
+
+      //   //TODO Handle getDependecies error
+      //   //if(err) cb(new Error('Could not get possible packages'))
+
+      //   finder.getClockMembersList(function (err, members) {
+
+      //     function memberPackages(element) {
+      //       var repoUrlParts = element.split('/')
+      //         , repoUser = repoUrlParts[repoUrlParts.length - 2]
+      //       return members.indexOf(repoUser) > -1
+      //     }
+
+      //     actualPackages = possiblePackages.filter(memberPackages)
+      //     console.log(3, possiblePackages)
+
+      //     cb(null, actualPackages)
+        
+      //   })
+
+      // })
     })
   }
 
