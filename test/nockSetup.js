59aad4ac6f6d61bb73948a69c93883bc4adef688
Owen Garland <owen.garland@clock.co.uk>
2015-07-31 10:15:36 +0100

    Fixed litning errors in tests

--- test/nockSetup.js
+++ test/nockSetup.js
@@ -2,43 +2,42 @@ var nock = require('nock')
 
 module.exports = function() {
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .persist()
   .get('/orgs/clocklimited/members')
-  .query({page: 1, 'per_page': 100})
+  .query({ page: 1, 'per_page': 100 })
   .replyWithFile(200, __dirname + '/responses/members.json')
 
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .persist()
   .get('/repos/bag-man/process-game/contents/package.json')
   .replyWithFile(200, __dirname + '/responses/errorpackage.json')
 
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .persist()
   .get('/repos/maael/gw2-api-wrapper/contents/package.json')
   .replyWithFile(200, __dirname + '/responses/package.json')
 
-
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .get('/teams/152302/repos')
-  .query({page: 1, 'per_page': 100})
+  .query({ page: 1, 'per_page': 100 })
   .replyWithFile(200, __dirname + '/responses/clock_repos.json')
 
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .persist()
   .get('/teams/152302/repos')
-  .query({page: 1, 'per_page': 100})
+  .query({ page: 1, 'per_page': 100 })
   .replyWithFile(200, __dirname + '/responses/repos.json')
 
   nock('https://api.github.com')
-  .defaultReplyHeaders({'Content-Type': 'application/json'})
+  .defaultReplyHeaders({ 'Content-Type': 'application/json' })
   .persist()
   .get('/users/tj/repos')
-  .query({page: 1, 'per_page': 100})
+  .query({ page: 1, 'per_page': 100 })
   .replyWithFile(200, __dirname + '/responses/userrepos.json')
 
   console.log('Nock routes started')
@@ -46,7 +45,7 @@ module.exports = function() {
   return nock
 }
 
-/* Test a nock route 
+/* Test a nock route
 var http = require('https')
 
 http.get('https://api.github.com/teams/152302/repos?page=1&per_page=100', function(res) {
