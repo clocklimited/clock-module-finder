cdaa7a29b90b579ede8bfc3ae43b9da766d401bc
Owen Garland <owen.garland@clock.co.uk>
2015-08-03 16:46:19 +0100

    Generated new report with table and david-dm check

--- showReport.js
+++ showReport.js
@@ -1,17 +1,16 @@
 var moduleFinder = require('./index.js')
+  , finder = require('./lib/finder.js')()
+  , async = require('async')
+  , fs = require('fs')
 
 moduleFinder.modules().find(
   { includeList: [ 'tomgco' ] }
 , function (err, res) {
-    console.log(moduleFinder.report().get(err, res))
+    async.map(res, finder.getDavid, saveReport)
   }
 )
 
-var finder = require('./lib/finder.js')()
-finder.getDavid({ user: 'bag-man', repo: 'nodeup', file: 'README.md' }, function(err, res) {
-  console.log('Does nodeup use david-dm? ' + res)
-})
-
-finder.getDavid({ user: 'clocklimited', repo: 'clock-module-finder', file: 'README.md' }, function(err, res) {
-  console.log('Does clock-module-finder use david-dm? ' + res)
-})
+function saveReport(err, res) {
+  var fd = fs.openSync(__dirname + '/report.md', 'w')
+  fs.writeSync(fd,moduleFinder.report().get(err, res))
+}
