255dc9a865a306eb07dfbbcea60b51668cfacb02
Owen Garland <owen.garland@clock.co.uk>
2015-08-03 13:51:34 +0100

    Updated api tests npm test does not work yet with nock

--- test/api.test.js
+++ test/api.test.js
@@ -1,12 +1,24 @@
 var assert = require('assert')
-  , api = require('../api')()
+  , api = require('../lib/api')()
   , npm = require('npm')
+  , nock = require('nock')
 
 describe('api', function () {
   describe('Test calls to github API for organisation members', function () {
     describe('#getOrgMembers', function () {
       it('should get multiple pages of members for nodejs', function (done) {
-        this.timeout(0)
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/orgs/nodejs/members')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/nodejs-users1.json')
+
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/orgs/nodejs/members')
+        .query({ page: 2, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/nodejs-users2.json')
+
         api.getOrgMembers({ org: 'nodejs' }, function (err, members) {
           assert.equal(members.length === 147, true, 'Has got all pages')
           done()
@@ -14,12 +26,15 @@ describe('api', function () {
       })
 
       it('should get the list of current Clock members', function (done) {
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/orgs/clocklimited/members')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/members.json')
+
         api.getOrgMembers({ org: 'clocklimited' }, function (err, members) {
-          // TODO : change to check against static nock & specific length
-          assert.equal((typeof members), 'object')
-          assert.equal(members.length > 0, true)
-          assert.equal(typeof members[0], 'object')
-          assert.equal(typeof members[0].login, 'string')
+          assert.equal(members.length === 13, true, 'should return 28 members')
+          assert.equal(members[0].login === 'ArnIIe', true, 'should return ArnIIe as first member')
           done()
         })
       })
@@ -29,18 +44,33 @@ describe('api', function () {
   describe('Test calls to github API for organisation and user repositories', function () {
     describe('#getClockRepos', function () {
       it('should get the short list of Clock repos', function (done) {
-        this.timeout(0)
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/teams/152302/repos')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/clock-repos.json')
+
         api.getRepos({ teamId: '152302', type: 'private' }, function(err, repos) {
-          // check against expected values
-          assert.equal(typeof repos, 'object', 'Repo list is not an object')
+          assert.equal(repos[0].name === 'jshint-config', true, 'expected first repo to be jshint-config')
+          assert.equal(repos.length === 58, true, 'should return 58 repos')
           done()
         })
       })
 
       it('should get the multiple pages of google repos', function (done) {
-        this.timeout(0)
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/teams/7378196/repos')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/google-repos1.json')
+
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/teams/7378196/repos')
+        .query({ page: 2, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/google-repos2.json')
+
         api.getRepos({ teamId: '7378196' }, function (err, repos) {
-          assert.equal(typeof repos, 'object', 'Repo list is an object')
           assert.equal(repos.length === 160, true, 'Has not used multiple pages')
           done()
         })
@@ -49,16 +79,32 @@ describe('api', function () {
 
     describe('#getUserRepos', function () {
       it('should get list of repos', function (done) {
-        this.timeout(0)
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/users/tj/repos')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/user-repos.json')
+
         api.getRepos({ user: 'tj' }, function (err, repos) {
-          // more specific tests
-          assert.equal(typeof repos, 'object', 'Repo list is not an object')
+          assert.equal(repos.length === 99, true, 'Expected 99 repos')
+          assert.equal(repos[0].name === 'array', true, 'expected first repo to be array')
           done()
         })
       })
 
       it('should get multiple pages of tj\'s repos', function (done) {
-        this.timeout(0)
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/users/tj/repos')
+        .query({ page: 1, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/user-repos1.json')
+
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/users/tj/repos')
+        .query({ page: 2, 'per_page': 100 })
+        .replyWithFile(200, __dirname + '/responses/user-repos2.json')
+
         api.getRepos({ user: 'tj' }, function(err, repos) {
           assert.equal(typeof repos, 'object', 'Repo list is an object')
           assert.equal(repos.length === 120, true, 'Has not used multiple pages')
@@ -71,26 +117,42 @@ describe('api', function () {
   describe('Test calls to NPM registry', function () {
     describe('#getPackageJson', function () {
       it('should get the package json for a repo', function (done) {
+        nock('https://api.github.com')
+        .defaultReplyHeaders({ 'Content-Type': 'application/json' })
+        .get('/repos/maael/gw2-api-wrapper/contents/package.json')
+        .replyWithFile(200, __dirname + '/responses/package.json')
+
         api.getPackageJson({ user: 'maael', repo: 'gw2-api-wrapper' }, function (err, packageJson) {
-          // more specific, check in package.json
-          assert.equal(err, null, 'An error is returned')
-          assert.equal(typeof packageJson, 'object', 'packageJson wasn\'t an object')
+          assert.equal(packageJson.name, 'package.json', 'package.json not found')
+          assert.equal(typeof packageJson.content, 'string', 'No content in file')
           done()
         })
       })
     })
 
     describe('#getPackageRepo', function () {
+      // nock.recorder.rec()
       it('should get the repo of the package', function (done) {
-        // more specific
+        nock('http://npm.clockte.ch:80')
+        .persist()
+        .get('/mocha')
+        .replyWithFile(304, __dirname + '/responses/npm-mocha.json')
+
+        /*
+        nock('https://registry.npmjs.org:443')
+        .persist()
+        .get('/mocha')
+        .replyWithFile(304, __dirname + '/responses/npm-mocha.json') */
+
         npm.load({}, function () {
           api.getPackageRepo('mocha', function (err, packageRepo) {
-            assert.equal(err, null, 'An error is returned')
-            assert.equal(typeof packageRepo, 'object', 'PackageRepo wasn\'t an object')
+            // Perform tests here.
+            console.log(packageRepo)
             done()
           })
         })
       })
+      // console.log(nock.recorder.play())
     })
   })
 })
